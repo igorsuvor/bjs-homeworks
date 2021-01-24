99b823ce392dd3b9e15172fa0953ba404de5fe3b
Igor Suvorov <2195183@mail.ru>
2021-01-25 01:46:39 +0500

    Completed homeworks 1.2-extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,11 +1,42 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+'use strict';
+
+function getResult(a,b,c) {
+  let D = (Math.pow(b,2) - 4 * a * c);
+  let x1 = (- b + Math.sqrt(D)) / (2 * a);
+  let x2 = (- b - Math.sqrt(D)) / (2 * a);
+
+    if (D < 0) {
+      let x = [];
+        return x;
+    } 
+     
+    else if (D === 0) {
+      let x = [x1];
+        return x;
+    }
+
+    else if (D > 0) {
+      let x = [x1, x2];
+        return x;
+    }
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    
+    // let quantityEstimation = (marks.lenght);
+     
+    // if (quantityEstimation == undefined) {
+    //   return 0;
+    // } 
+    
+    if (marks.lenght == []) {
+       return 0;
+     } 
+    else if (marks.lenght > 5) {
+        return 'Оценок больше 5!';
+      } 
+
+    //return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
