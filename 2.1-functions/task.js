e26ea63e65db40bfbcccdf4e8ae9f6ec2c2d7ecd
Igor Suvorov <2195183@mail.ru>
2021-03-15 03:45:53 +0500

    Complete homeworks 2.1-functions_4

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -34,86 +34,85 @@ function showSolutionsMessage(a, b, c) {
 
 
 function getAverageScore(data) {
-  for (let prop in data) {
-    let value = data[prop];
-    let nSum = 0;
-    let average;
-    let countElementsObj = Object.keys(data).length;
-      nSum += getAverageMark();
-      /* Здесь у меня не получается сделать, а именно сложить средние
-      значения всех предметов и выести это значение отдельной строкой. 
-      Подскажите в каком направлении двигаться, что я не правильно сделал?
-      */
-      average = nSum / countElementsObj;
+  let averageSum = 0;
 
-  function getAverageMark(marks) {
-    marks = value;
-  if (marks.length == 0) {
-    return 0;
-  }
-    let sum = 0;
-      for(let i = 0; i < marks.length; i++) {
-        sum += marks[i];
+    for (let prop in data) {
+      function getAverageMark(marks) {
+        marks = data[prop];
+
+      if (marks.length == 0) {
+        return 0;
       }
-  return sum / marks.length;
- }
- console.log(`${prop}: ${getAverageMark()}`);
- console.log(`average: ${average}`);
- }
+      let sum = 0;
+        for(let i = 0; i < marks.length; i++) {
+          sum += marks[i];
+        }
+      return sum / marks.length;
+      }
+    averageSum += getAverageMark(Object.values(data));
+
+    console.log(`${prop}: ${getAverageMark()}`);
+    }
+    
+  let average = averageSum / Object.values(data).length;
+    if (Object.values(data).length === 0) {
+      average = 0;
+  }
+    console.log(`average: ${average}`);
+ 
 }
 console.log(getAverageScore({
-  algebra: [ 2, 4, 5, 2, 3, 4 ],
-  geometry: [ 2, 4, 5 ],
-  russian: [ 3, 3, 4, 5 ],
-  physics: [ 5, 5 ], 
-  music: [ 2, 2, 6 ], 
-  english: [ 4, 4, 3 ],
-  poetry: [ 5, 3, 4 ], 
-  chemistry: [ 2 ],
-  french: [ 4, 4 ]
-  }));
+ algebra: [ 2, 4, 5, 2, 3, 4 ],
+ geometry: [ 2, 4, 5 ],
+ russian: [ 3, 3, 4, 5 ],
+ physics: [ 5, 5 ], 
+ music: [ 2, 2, 6 ], 
+ english: [ 4, 4, 3 ],
+ poetry: [ 5, 3, 4 ], 
+ chemistry: [ 2 ],
+ french: [ 4, 4 ]
+ }));
+ 
   
 
-
-  function getPersonData(secretData) {
-    secretData = getDecodedValue();
+function getPersonData(secretData) {
+  function getDecodedValue(secret) {
     
-    function getDecodedValue(secret) {
-      secret = {
-        firstName: 0,
-        lastName: 1
-      }
-      // for (let prop in secret) {
-      //   if (secret[prop] === 0) {
-      //   return secret[prop] = 'Родриго';
-        
-      //   }
-      /* В этом задании не могу понять, как 0 и 1 присвоить другие значения.
-      Возможно условной конструкцией if else, допустим если secret.firstNamе=0, то 0=“Родриго”.
-      Или обойти объект циклом for in и если secret[prop]=0, то то 0="Родриго". Какой способ исполь-
-      зовать? Или что-то другое делать?
-      */
-
-      return secret;
-    }
-    return secretData;
+    if (secret === 0) {
+      decodedValue = 'Родриго';
+    } 
+  
+    else {
+      decodedValue = 'Эмильо';
+    } 
+  return decodedValue;
   }
-  console.log(getPersonData({
-  aaa: 0,
-  bbb: 0
-  }));
-  
-  console.log(getPersonData({
-  aaa: 1,
-  bbb: 0
-  }));
-  
-  console.log(getPersonData({
-  aaa: 0,
-  bbb: 1
-  }));
-  
-  console.log(getPersonData({
-  aaa: 1,
-  bbb: 1
-  }));
+ 
+  decodedName = {
+      firstName: getDecodedValue(secretData.aaa),
+      lastName: getDecodedValue(secretData.bbb)
+  }
+ 
+  return decodedName;
+}
+
+
+console.log(getPersonData({
+aaa: 0,
+bbb: 0
+}));
+
+console.log(getPersonData({
+aaa: 1,
+bbb: 0
+}));
+
+console.log(getPersonData({
+aaa: 0,
+bbb: 1
+}));
+
+console.log(getPersonData({
+aaa: 1,
+bbb: 1
+}));
