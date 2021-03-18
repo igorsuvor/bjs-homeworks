a24537579ede7364ecda50baee11b985fd27bdba
Igor Suvorov <2195183@mail.ru>
2021-03-18 17:06:37 +0500

    Complete homeworks 2.1-functions_5

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,18 +1,21 @@
+// ЗАДАЧА № 1
+
 function getSolutions(a, b, c) {
   let D = (Math.pow(b, 2) - 4 * a * c);
-  let x1 = (- b + Math.sqrt(D)) / (2 * a);
-    
+      
   if (D === 0) {
-    return { D: D, roots: [x1] };
+    let x1 = (-b / 2 * a);
+      return { D: D, roots: [x1] };
   }
 
   else if (D > 0) {
+    let x1 = (- b + Math.sqrt(D)) / (2 * a);
     let x2 = (- b - Math.sqrt(D)) / (2 * a);
       return { D: D, roots: [x1, x2] };
   }
 
   return { D: D, roots: [] };
-}
+};
   
 function showSolutionsMessage(a, b, c) {
   let result = getSolutions(a, b, c);
@@ -29,38 +32,37 @@ function showSolutionsMessage(a, b, c) {
   else if ((result.D) > 0) {
     console.log(`Уравнение имеет два корня. X₁ = ${(result.roots[0])}, X₂ = ${(result.roots[1])}`);
   }
-}
+};
 
 
+// ЗАДАЧА № 2
 
 function getAverageScore(data) {
-  let averageSum = 0;
+  let result = {};
+  
+  for (let prop in data) {
+    let averageScore = getAverageMark(data[prop]);
+      result[prop] = averageScore;
+  };
 
-    for (let prop in data) {
-      function getAverageMark(marks) {
-        marks = data[prop];
+  let average = getAverageMark(Object.values(result));
+    result.average = average;
 
-      if (marks.length == 0) {
-        return 0;
-      }
-      let sum = 0;
-        for(let i = 0; i < marks.length; i++) {
-          sum += marks[i];
-        }
-      return sum / marks.length;
-      }
-    averageSum += getAverageMark(Object.values(data));
+  return result;
+};
 
-    console.log(`${prop}: ${getAverageMark()}`);
-    }
-    
-  let average = averageSum / Object.values(data).length;
-    if (Object.values(data).length === 0) {
-      average = 0;
-  }
-    console.log(`average: ${average}`);
+function getAverageMark(marks) {
+  if (marks.length == 0) {
+    return 0;
+  };
+  let sum = 0;
+  for(let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  };
  
-}
+  return sum / marks.length;
+};
+
 console.log(getAverageScore({
  algebra: [ 2, 4, 5, 2, 3, 4 ],
  geometry: [ 2, 4, 5 ],
@@ -73,29 +75,28 @@ console.log(getAverageScore({
  french: [ 4, 4 ]
  }));
  
-  
+
+
+
+// ЗАДАЧА № 3
 
 function getPersonData(secretData) {
-  function getDecodedValue(secret) {
-    
-    if (secret === 0) {
-      decodedValue = 'Родриго';
-    } 
-  
-    else {
-      decodedValue = 'Эмильо';
-    } 
-  return decodedValue;
-  }
- 
   decodedName = {
-      firstName: getDecodedValue(secretData.aaa),
-      lastName: getDecodedValue(secretData.bbb)
-  }
- 
+    firstName: getDecodedValue(secretData.aaa),
+    lastName: getDecodedValue(secretData.bbb)
+  };
   return decodedName;
-}
+};
 
+function getDecodedValue(secret) {
+  if (secret === 0) {
+    decodedValue = 'Родриго';
+  }
+  else {
+    decodedValue = 'Эмильо';
+  };
+  return decodedValue;
+};
 
 console.log(getPersonData({
 aaa: 0,
