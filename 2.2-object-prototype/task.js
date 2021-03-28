450fae7e9033af68c6911fd2312afe99e5ba1712
Igor Suvorov <2195183@mail.ru>
2021-03-29 02:49:47 +0500

    Complete homework 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,31 @@
-//String.prototype.isPalindrome - для задачи №1
-
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+// Задача № 1.
+String.prototype.isPalindrome = function () {
+  return this.toLowerCase().split(' ').join('').split('').reverse().join('') === this.toLowerCase().split(' ').join('').split('').join('') ? true : false;
 }
 
+
+// Задача № 2.
+function getAverageMark(marks) {
+    if (marks.length == 0) {
+      return 0;
+    }
+    let sum = 0;
+      for(let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+      }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
+}
+
+
+// Задача № 3.
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = Date.now();
+  birthday = Date.parse(birthday);
+  let diff = now - birthday;
+  let age = diff / (1000 * 60 * 60 * 24 * 365.25);
+  let verdict = age >= 18 ? true : false;
+
+  return verdict;
 }
